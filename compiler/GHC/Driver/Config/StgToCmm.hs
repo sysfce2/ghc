3dbbe95d4510162b492d87f7e383a188c04a0996
Jaro Reinders <jaro.reinders@gmail.com>
2023-06-30 16:26:04 +0200

    Add AllowBigQuot option to StgToCmm

--- compiler/GHC/Driver/Config/StgToCmm.hs
+++ compiler/GHC/Driver/Config/StgToCmm.hs
@@ -52,6 +52,7 @@ initStgToCmmConfig dflags mod = StgToCmmConfig
   , stgToCmmDoTagCheck    = gopt Opt_DoTagInferenceChecks  dflags
   -- backend flags
   , stgToCmmAllowBigArith             = not ncg || platformArch platform == ArchWasm32 || platformArch platform == ArchX86
+  , stgToCmmAllowBigQuot              = not ncg || platformArch platform == ArchWasm32
   , stgToCmmAllowQuotRemInstr         = ncg  && (x86ish || ppc)
   , stgToCmmAllowQuotRem2             = (ncg && (x86ish || ppc)) || llvm
   , stgToCmmAllowExtendedAddSubInstrs = (ncg && (x86ish || ppc)) || llvm
